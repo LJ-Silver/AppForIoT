05f5268b4b186a1e20a2acaaf874b4b924becce8
root <root@debian.debian_lan>
2017-06-11 23:38:28 +0200

    test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
- var express= require("express");
+var express= require("express");
+var bodyParser = require("body-parser");
+
 
 var app = express();
 
@@ -9,3 +11,90 @@ app.get('/',function (req,res){
 
 
 app.listen(5000);
+
+
+var games = [];
+
+app.get("/api-test/games/loadInitialData", (req,res) =>{
+	games = [{ name : "cod", PEGI : "18"},{ name : "GTA", PEGI : "18"}];
+	console.log("2 elements initialized.");
+	res.sendStatus(201);
+});
+
+app.get("/api/sandbox/games/", (req,res) =>{
+	console.log("New GET of all resources.");
+	res.send(games);
+});
+
+app.get("/api/sandbox/games/:name", (req,res) =>{
+	var name = req.params.name;
+	var aux = null;
+	for (var i = 0; i < games.length; i++) {
+		if (games[i].name == name){
+			aux = games[i];
+			res.send(aux);
+		}
+	}
+	if (aux == null) {
+		res.sendStatus(404);
+	}
+	console.log("New GET of resource "+name);
+	
+	
+});
+
+app.post("/api/sandbox/games", (req,res) =>{
+	var game = req.body;
+	games.push(game);
+	console.log("New POST of resource "+game.name);
+	res.sendStatus(201);
+});
+
+app.post("/api/sandbox/games/:name", (req,res) =>{
+	res.sendStatus(405);
+});
+
+app.put("/api/sandbox/games/:name", (req,res) =>{
+	var game = req.body;
+	var aux = null;
+	for (var i = 0; i < games.length; i++) {
+		if (games[i].name == req.params.name) {
+			aux = games[i];
+			aux.name = game.name;
+			aux.PEGI = game.PEGI;
+			res.sendStatus(200);
+		}
+	}
+	if (aux == null) {
+		res.sendStatus(404);
+	}
+	console.log("New PUT of resource "+game.name);
+	
+});
+
+app.put("/api/sandbox/games", (req,res) =>{
+	res.sendStatus(405);
+});
+
+app.delete("/api/sandbox/games", (req,res) =>{
+	games = [];
+	console.log("You DELETED all games :(");
+	res.send(200);
+});
+
+app.delete("/api/sandbox/games/:name", (req,res) =>{
+	var name = req.params.name;
+	var aux = null;
+	for (var i = 0; i < games.length; i++) {
+		if (games[i].name == name){
+			aux = games[i];
+			games.splice(i,1);
+			res.send(200);
+		}
+	}
+	if (aux == null) {
+		res.sendStatus(404);
+	}
+	console.log("You deleted the game successfully.");
+	
+});
